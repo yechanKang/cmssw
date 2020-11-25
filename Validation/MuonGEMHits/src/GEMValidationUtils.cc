f78d6e2124f8c07e0f7346a7fd3d540733b64a0b
yeckang <yechan.kang@cern.ch>
2020-11-25 14:00:49 +0100

    change name and title convention for GEM validation

--- Validation/MuonGEMHits/src/GEMValidationUtils.cc
+++ Validation/MuonGEMHits/src/GEMValidationUtils.cc
@@ -2,18 +2,24 @@
 
 #include "TString.h"
 
-TString GEMUtils::getSuffixName(Int_t region_id) { return TString::Format("_re%d", region_id); }
+TString GEMUtils::getSuffixName(Int_t region_id) { return TString::Format("_RE%+d", region_id); }
 
 TString GEMUtils::getSuffixName(Int_t region_id, Int_t station_id) {
-  return TString::Format("_re%d_st%d", region_id, station_id);
+  char sign = region_id > 0 ? '+' : '-';
+  station_id = abs(station_id * 10 + (station_id != 0));
+  return TString::Format("_GE%c%d", sign, station_id);
 }
 
 TString GEMUtils::getSuffixName(Int_t region_id, Int_t station_id, Int_t layer_id) {
-  return TString::Format("_re%d_st%d_la%d", region_id, station_id, layer_id);
+  char sign = region_id > 0 ? '+' : '-';
+  station_id = abs(station_id * 10 + (station_id != 0));
+  return TString::Format("_GE%c%d_L%d", sign, station_id, layer_id);
 }
 
 TString GEMUtils::getSuffixName(Int_t region_id, Int_t station_id, Int_t layer_id, Int_t roll_id) {
-  return TString::Format("_re%d_st%d_la%d_ro%d", region_id, station_id, layer_id, roll_id);
+  char sign = region_id > 0 ? '+' : '-';
+  station_id = abs(station_id * 10 + (station_id != 0));
+  return TString::Format("_GE%c%d_L%d_R%d", sign, station_id, layer_id, roll_id);
 }
 
 TString GEMUtils::getSuffixName(const ME2IdsKey& key) {
@@ -31,18 +37,24 @@ TString GEMUtils::getSuffixName(const ME4IdsKey& key) {
   return getSuffixName(region_id, station_id, layer_id, roll_id);
 }
 
-TString GEMUtils::getSuffixTitle(Int_t region_id) { return TString::Format(" Region %d", region_id); }
+TString GEMUtils::getSuffixTitle(Int_t region_id) { return TString::Format(" Region %+d", region_id); }
 
 TString GEMUtils::getSuffixTitle(Int_t region_id, Int_t station_id) {
-  return TString::Format(" Region %d Station %d", region_id, station_id);
+  char sign = region_id > 0 ? '+' : '-';
+  station_id = abs(station_id * 10 + (station_id != 0));
+  return TString::Format(" GE%c%d", sign, station_id);
 }
 
 TString GEMUtils::getSuffixTitle(Int_t region_id, Int_t station_id, Int_t layer_id) {
-  return TString::Format(" Region %d Station %d Layer %d", region_id, station_id, layer_id);
+  char sign = region_id > 0 ? '+' : '-';
+  station_id = abs(station_id * 10 + (station_id != 0));
+  return TString::Format(" GE%c%d_L%d", sign, station_id, layer_id);
 }
 
 TString GEMUtils::getSuffixTitle(Int_t region_id, Int_t station_id, Int_t layer_id, Int_t roll_id) {
-  return TString::Format(" Region %d Station %d Layer %d Roll %d", region_id, station_id, layer_id, roll_id);
+  char sign = region_id > 0 ? '+' : '-';
+  station_id = abs(station_id * 10 + (station_id != 0));
+  return TString::Format(" GE%c%d_L%dR_%d", sign, station_id, layer_id, roll_id);
 }
 
 TString GEMUtils::getSuffixTitle(const ME2IdsKey& key) {
